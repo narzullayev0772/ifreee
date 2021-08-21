77e48dc0c5d6dbb8a9d35cb078752949a2a5e43b
Developer0772 <alishernarzullayev7777@gmail.com>
2021-08-21 17:13:28 +0500

    pushing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const {Server} = require('socket.io');
 const io = new Server(server);
 
 
-const PORT= 3000 || process.env.PORT;
+const PORT= process.env.PORT||3000;
 app.use(express.static(__dirname + '/public'))
 
 app.get('/',(req,res)=>{
